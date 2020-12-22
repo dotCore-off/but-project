7b6dadea6fa79d0db57051a753acb0f0875e69b3
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2020-12-22 19:05:00 +0100

    Footer avant CSS

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../media/logoIUT1.png';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="Facebook">
+                <a href="https://www.facebook.com/IUTRoanne">Feed Facebook</a>
+            </div>
+            <div className="central">
+                <div className="Logo">
+                    <img src={logo} />
+                </div>
+                <div className="SiteIUT">
+                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/index.html">Site de l'IUT</a>
+                </div>
+                <div className="MentionLegales">
+                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/liens-de-bas-de-page/mentions-legales-2.html">Mentions Légales</a>
+                </div>
+            </div>
+            <div className="Instagram">
+                <a href="https://www.instagram.com/iutroanne/">Feed Instagram</a>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       Home
       <Navigation />
+      <Footer />
     </div>
   );
 };
