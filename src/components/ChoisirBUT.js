f99caad0b53d8f856dc5611f1da050d28cdfb02f
NathanRodet <64322615+NathanRodet@users.noreply.github.com>
2020-12-27 23:22:29 +0100

    ChoisirBUT
    few changes such as list

--- src/components/ChoisirBUT.js
+++ src/components/ChoisirBUT.js
@@ -7,22 +7,39 @@ const ChoisirBUT = () => {
     return (
         <div className="choisirBUT">
             <div className="avantagesIUT">
-                <h1>Nos avantages !</h1>
-                <p>
-                -Taille Humaine<br />
-                -Proche des commerces<br />
-                -Proche des habitats<br />
-                -Transport en commun<br />
-                -Associations sportives/vie étudiante<br />(maison des étudiants)<br />
-                -Du soutien et une forte communication<br /> entre RT1 et RT2<br />
-                -Un RestoU (3.30€/repas + qualité car il est relié à l'hôpital)<br /> et un BistroU (Crous) disponible pour les repas du midi.<br />
-                -BU reliée avec Saint-Étienne et Lyon présente dans l'IUT<br />
-                -Tutorat, chaque élève est suivi par un professeur pour sa réussite<br />
-                -Beaucoup de matériel mis en place pour le RT (TP et TD)<br />
-                </p>
+                <h2>Nos avantages</h2>
+                    <ul>
+                        <li>
+                        Formation en 3 ans
+                        </li>
+                        <li>
+                        Proche des commerces et des habitats
+                        </li>
+                        <li>
+                        Transport en communs
+                        </li>
+                        <li>
+                        Associations sportives/vie étudiante (maison des étudiants)
+                        </li>
+                        <li>
+                        Du soutien et une forte communication entre RT1 et RT2
+                        </li>
+                        <li>
+                        Un RestoU (3.30€/repas + qualité car il est relié à l'hôpital) et un BistroU (Crous) disponible pour les repas du midi.
+                        </li>
+                        <li>
+                        BU reliée avec Saint-Étienne et Lyon présente dans l'IUT
+                        </li>
+                        <li>
+                        Tutorat, chaque élève est suivi par un professeur pour sa réussite
+                        </li>
+                        <li>
+                        Beaucoup de matériel mis en place pour le RT (TP et TD)
+                        </li>
+                    </ul>
             </div>
             <div className="certifications">
-                <h1>Nos certifications !</h1>
+                <h2>Nos certifications</h2>
                 <div className="cisco">
                     <img src={cisco} alt="Cerfication CCNA cisco" className="imgcisco"></img>
                 </div>
@@ -31,15 +48,26 @@ const ChoisirBUT = () => {
                 </div>
             </div>
             <div className="avantagesBUT">
-                <h1>Les avantages du BUT !</h1>
-                <p className paragavantageBUT>-Formation en 3 ans<br />
-                    -Différents choix de parcours<br />
-                    -3 blocs de compétence communs et 5 additionnels<br />
-                    -Compétence dans le domaine des réseaux et télécoms<br />
-                    -Filière avec beaucoup d'emploi-Stage de deux mois en 3e année<br />
-                </p>
+                <h2>Les avantages du BUT</h2>
+                    <ul>
+                        <li>
+                        Formation en 3 ans
+                        </li>
+                        <li>
+                        Différents choix de parcours
+                        </li>
+                        <li>
+                        Filière avec beaucoup d'emploi-Stage de deux mois en 3e année
+                        </li>
+                        <li>
+                        3 blocs de compétence communs et 5 additionnels
+                        </li>
+                        <li>
+                        Compétence dans le domaine des réseaux et télécoms
+                        </li>
+                    </ul>
                 <div className="infoBUT">
-                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/tous-les-faits-marquants/annee-2020-2021-2/zoom-sur/but.html" target= "_blank">
+                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/tous-les-faits-marquants/annee-2020-2021-2/zoom-sur/but.html" target= "_blank" rel="noreferrer" >
                     Pour en savoir plus sur le BUT !
                     </a>
                 </div>
