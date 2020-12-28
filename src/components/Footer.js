c0a2f4ace30994f1ea97febcaec2a905b61b11d5
NathanRodet <64322615+NathanRodet@users.noreply.github.com>
2020-12-28 23:13:57 +0100

    Home finish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-
+import { NavLink } from 'react-router-dom';
 import logo from '../media/logoIUT1.png';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="Facebook">
-                <a href="https://www.facebook.com/IUTRoanne" target = "_blank" className="afooter"><i class="fab fa-facebook-f"></i></a>
-            </div>
-            <div className="Instagram">
-                <a href="https://www.instagram.com/iutroanne/" target = "_blank" className="afooter"><i class="fab fa-instagram"></i></a>
-            </div>
-            <div className="Logo">
-                <a href="https://iut-roanne.univ-st-etienne.fr/fr/index.html" target = "_blank" className="afooter">
-                    <img src={logo} alt="Logo IUT" className="image" href="https://iut-roanne.univ-st-etienne.fr/fr/index.html" target = "_blank"></img>
-                </a>
-            </div>
-            <div className="Connexion">
-                <div className="bouton">
-                <Button>Se connecter</Button>
-                </div>
-            </div>
-            <div className="MentionLegales">
-                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/liens-de-bas-de-page/mentions-legales-2.html" target = "_blank" className="afooter">Mentions Légales</a>
-            </div>
-            
-            
-
+            <ul>
+                <li>
+                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/index.html" target = "_blank" rel="noreferrer" className="afooter">
+                        <img src={logo} alt="Logo IUT" className="image" href="https://iut-roanne.univ-st-etienne.fr/fr/index.html" target = "_blank"></img>
+                    </a>
+                </li>
+                <li id="adminFooter">
+                    <NavLink exact to="/" activeClassName="navActive">
+                         Administration
+                    </NavLink>
+                </li>
+                <li id="fbFooter">
+                    <a href="https://www.facebook.com/IUTRoanne" target = "_blank" rel="noreferrer" className="afooter"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li id="instaFooter">
+                    <a href="https://www.instagram.com/iutroanne/" target = "_blank" rel="noreferrer" className="afooter"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="https://iut-roanne.univ-st-etienne.fr/fr/liens-de-bas-de-page/mentions-legales-2.html" target = "_blank" rel="noreferrer" className="afooter">Mentions Légales</a>
+                </li>
+            </ul>
         </div>
     );
 };
