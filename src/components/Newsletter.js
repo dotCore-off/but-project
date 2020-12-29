8f99d47a834eada120018e858a6b9ceaea525241
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2020-12-29 13:54:57 +0100

    Newsletter V1

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Newsletter = () => {
-    return(
-        <div className="newsletterWrapper">
-            <div className="newletterBox">
-                <h2>Inscrivez vous à notre Newsletter !</h2>
-                <p>Inscrivez-vous à notre Newsletter pour recevoir des infos de l'IUT</p>
-                <form>
-                    <div>
-                        <input type="text" value="Votre Mail" name=""></input>
-                        <input type="submit" value="S'inscrire" name=""></input>
-                    </div>
-                </form>
-
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="newsletterWrapper">
+      <div className="newletterBox">
+        <h2>Inscrivez vous à notre Newsletter !</h2>
+        <p>
+          Inscrivez-vous à notre Newsletter pour recevoir des infos de l'IUT
+        </p>
+        <form method="post">
+          <div>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Votre mail" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+            <input type="submit" value="S'inscrire" name=""></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
