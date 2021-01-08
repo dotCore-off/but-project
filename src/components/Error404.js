4f76f6ff7b6872702fcd2ba85961d083a5df39e1
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-08 17:23:54 +0100

    error404 V2

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import error404 from '../media/error404.svg';
+import error404 from "../media/error404.svg";
 const Error404 = () => {
   return (
-    <div class="container">
-      <img src={error404} alt="React Logo" />
-      <h3>Oops, you broke the internet</h3>
+    <div className="error404">
+      <div className="divErrorLogo">
+        <img src={error404} alt="React Logo" className="errorLogo" />
+      </div>
+      <h2>Oops, you broke the internet</h2>
       <p>
         Just kidding. But it seems Robo, your personal robot, can't find the
         page you're looking for. Please make sure you've entered the correct
-        address, or return to Home. </p>
-        <NavLink exact to="/" activeClassName="navActive">
-           Home
-        </NavLink>
+        address, or return to Home.{" "}
+      </p>
+      <NavLink exact to="/" activeClassName="navActive">
+        <a href="#" class="btn-flip" data-back="Back" data-front="Home"></a>
+      </NavLink>
     </div>
   );
 };
