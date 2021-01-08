e8ff40bd83dbb626883e34daa8b245b4bcd1d09d
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-08 18:04:04 +0100

    node icon

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import error404 from "../media/error404.svg";
+
+import error404 from "../media/error/error404.svg";
+import HomeIcon from '@material-ui/icons/Home';
+
 const Error404 = () => {
   return (
     <div className="error404">
@@ -8,13 +11,14 @@ const Error404 = () => {
         <img src={error404} alt="React Logo" className="errorLogo" />
       </div>
       <h2>Oops, you broke the internet</h2>
-      <p>
+      <p className="textError">
         Just kidding. But it seems Robo, your personal robot, can't find the
         page you're looking for. Please make sure you've entered the correct
-        address, or return to Home.{" "}
+        address, or return to Home.
       </p>
       <NavLink exact to="/" activeClassName="navActive">
-        <a href="#" class="btn-flip" data-back="Back" data-front="Home"></a>
+        <a class="btn-flip" data-back="Home" data-front={<HomeIcon />} rel="noreferrer" />
+    <HomeIcon />
       </NavLink>
     </div>
   );
