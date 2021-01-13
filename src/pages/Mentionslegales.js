02ff15b6fd8841936b8ad7f696d63816785d9ff3
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-13 08:50:00 +0100

    mentions légales

--- src/pages/Mentionslegales.js
+++ src/pages/Mentionslegales.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
+import React from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 const Mentionslegales = () => {
-    return(
-        <div className="mentions">
-            <Navigation />
-            Mentions legales
-            <Footer />
+  return (
+    <div className="mentions">
+      <Navigation />
+      <div className="mentionsContent">
+        <h1 id="mentionsTitle">Mentions Légales</h1>
         </div>
-    );
+      <Footer />
+    </div>
+  );
 };
 
 export default Mentionslegales;
