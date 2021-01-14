fbbe2c8933b851fd491cef620d8fdc369ed8109c
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-14 23:45:54 +0100

    Photos 360 V1

---
+++ src/components/Photos360.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+import { Pannellum} from "pannellum-react";
+
+import G02 from "../media/photos360/G02.jpg";
+import G07 from "../media/photos360/G07.jpg";
+import G04 from "../media/photos360/G04.jpg";
+
+const Photos360 = () => {
+  return (
+    <div className="photos360">
+      <h1>Les photos 360 de l'IUT</h1>
+      <div className="G07">
+        <Pannellum
+          width="80%"
+          height="500px"
+          image={G07}
+          pitch={10}
+          yaw={180}
+          hfov={110}
+          previewTitle="Salle G07"
+          title="Salle G07"
+          onLoad={() => {
+            console.log("panorama loaded");
+          }}
+        >
+          <Pannellum.Hotspot
+            type="info"
+            pitch={10}
+            yaw={-10}
+            text="Baie de brassage qui permet de cabler les différents équipements"
+          />
+
+          <Pannellum.Hotspot
+            type="info"
+            pitch={-30}
+            yaw={110}
+            text="Maquette d'un TP de câblage de première année"
+          />
+
+          <Pannellum.Hotspot
+            type="info"
+            pitch={-20}
+            yaw={-120}
+            text="TP de téléphonie de première année"
+          />
+        </Pannellum>
+      </div>
+
+      <div className="G04">
+        <Pannellum
+          width="80%"
+          height="500px"
+          image={G04}
+          pitch={10}
+          yaw={180}
+          hfov={110}
+          previewTitle="Salle G04"
+          title="Salle G04"
+          onLoad={() => {
+            console.log("panorama loaded");
+          }}
+        >
+          <Pannellum.Hotspot
+            type="info"
+            pitch={-20}
+            yaw={-150}
+            text="TP de signal de première année"
+          />
+        </Pannellum>
+      </div>
+
+      <div className="G02">
+        <Pannellum
+          width="80%"
+          height="500px"
+          image={G02}
+          pitch={10}
+          yaw={180}
+          hfov={110}
+          previewTitle="Salle G02"
+          title="Salle G02"
+          onLoad={() => {
+            console.log("panorama loaded");
+          }}
+        >
+          <Pannellum.Hotspot
+            type="info"
+            pitch={-10}
+            yaw={110}
+            text="Osciloscope et générateur de signal utilisé lors des TP d'électronique ou de signal"
+          />
+
+          <Pannellum.Hotspot
+            type="info"
+            pitch={-20}
+            yaw={-120}
+            text="TP de téléphonie de première année"
+          />
+        </Pannellum>
+      </div>
+    </div>
+  );
+};
+
+export default Photos360;
--- src/pages/Formation.js
+++ src/pages/Formation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Temoignages from '../components/Temoignages';
 import Footer from '../components/Footer';
+import Photos360 from '../components/Photos360';
 
 const Formation = () => {
   return (
@@ -9,6 +10,7 @@ const Formation = () => {
       <Navigation />
       <h1 id="formationTitle">La formation Réseaux et télécommunications</h1>
       <Temoignages />
+      <Photos360 />
       <Footer />
     </div>
   );
