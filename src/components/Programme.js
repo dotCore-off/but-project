8a3a1ebea226bcdfb7cb6401fa8bfcd4b0046c03
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-18 18:18:55 +0100

    ajout tab

--- src/components/Programme.js
+++ src/components/Programme.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Tabs from 'react-responsive-tabs';
+
+// IMPORTANT you need to include the default styles
+import 'react-responsive-tabs/styles.css';
+
+const année = [{ name: 'BUT 1', contenu: '...' }, { name: 'BUT 2', contenu: '...' }, { name: 'BUT 3', contenu: '...' }, { name: 'CYBER', contenu: '...' }, { name: 'ROM', contenu: '...' }];
+
+function getTabs() {
+  return année.map(année => ({
+    //key: index, // Optional. Equals to tab index if this property is omitted
+    tabClassName: 'tab', // Optional
+    panelClassName: 'panel', // Optional
+    title: année.name,
+    getContent: () => année.biography,
+  }));
+}
 
 const Programme = () => {
   return (
     <div className="divProgramme">
       <h2>Au programme !</h2>
+      <Tabs items={getTabs()} />
     </div>
   );
 };
