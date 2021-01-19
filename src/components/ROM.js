f41168cba9f981f14f9e8bbb3085d345f6fdb7bb
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-20 00:21:02 +0100

    ajout nouvel onglet navbar

--- src/components/ROM.js
+++ src/components/ROM.js
@@ -4,45 +4,48 @@ const ROM = () => {
   return (
     <div className="divROM">
       <div className="introROM">
-        <h3>Qu'est ce que le spécialisation Réseaux Opérateurs et Multimédia (ROM) ?</h3>
+        <h3>
+          Qu'est ce que le spécialisation Réseaux Opérateurs et Multimédia (ROM)
+          ?
+        </h3>
         <br></br>
         <p>
-          Cette spécialisation a pour but d'approfondir vos connaissances acquises au long des années précédentes
-        <br></br>
-      </p>
+          Cette spécialisation a pour but d'approfondir vos connaissances
+          acquises au long des années précédentes. <br />
+          Elle se déroule durant la deuxième et la troisième année.
+          <br></br>
+        </p>
       </div>
-      <br/><br/>
+      <br />
+      <br />
       <div className="...">
         <h3>Infrastructure et services réseaux opérateurs</h3>
-        <br/>
+        <br />
         <p>
-          Dans cette partie, vos connaissances en réseaux seront mises à l'épreuve.<br></br>
-          Par exemple, savez-vous virtualiser un réseau, comment marche le WIFI, qu'est ce qu'un routage ?<br></br>
-          ou encore vous êtes un flemmard stratégique et des scripts pourrez-vous aider dans cette quête.<br></br>
-          Voilà ce que peut vous offrir cette spécialisation. 
+          Dans cette partie, vos connaissances en réseaux seront mises à
+          l'épreuve.<br />
+          Par exemple, savez-vous comment fonctionne le Cloud, comment configurer un IPBx,
+          qu'est ce qu'un routage ?<br />
+          ou encore vous êtes un flemmard stratégique et des scripts
+          pourrez-vous aider dans cette quête.<br />
+          Voilà ce que peut vous offrir cette spécialisation.
         </p>
-        <br/>
+        <br />
         <div className="...">
-          <img src={"..."} 
-          alt="...." 
-          className="..."
-          ></img>
+          <img src={"..."} alt="...." className="..."></img>
         </div>
       </div>
       <div className="...">
-      <h3>Communication et vidéo :</h3>
-      <p>
-        Dans cette partie, vous mettrez en place un service ToIP téléphonique pour une entreprise<br></br>
-        ou encore un service de vidéo-surveillance via des matériels comme des IPbx.<br></br>
-        Vous serez en charge de configurer ce matériel et de la maintenance de cette installation. 
-      </p>
+        <h3>Communication et vidéo :</h3>
+        <p>
+          Dans cette partie, vous mettrez en place des service de visio
+          conférence pour les entreprise ou encore un service de vidéo-surveillance.<br />
+          Vous serez en charge d'administrer les services de communication des
+          entreprises (configuration avancée des IPBX par exemple).
+        </p>
       </div>
       <div className="...">
-        <img
-          src={"..."}
-          alt="..."
-          className="..."
-        ></img>
+        <img src={"..."} alt="..." className="..."></img>
       </div>
     </div>
   );
