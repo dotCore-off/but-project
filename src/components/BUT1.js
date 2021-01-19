b540ddef7ef42bce8782a61e5db85bff8863bf9e
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-01-19 20:18:22 +0100

    Updated some style's properties + Fixed some mistakes + Rewrote some texts

--- src/components/BUT1.js
+++ src/components/BUT1.js
@@ -6,22 +6,28 @@ import web from "../media/programme/BUT1/web.png";
 const BUT1 = () => {
   return (
     <div className="divBUT1">
-      <h3>Le déroulement de la première année</h3>
-      <p>
-        La première année est une introduction au monde des réseaux et
-        télécommunication. Elle a pour but de vous introduire dans cette univers
-        en vous apprenant les bases.
-      </p>
-      <span>Partie Réseaux :</span>
-      <p>
-        Dans la partie réseaux, vous allez apprendre ce qu'est un réseau,
-        comment ça fonctionne et comment configurer les différents appareil qui
-        composent un réseau local. <br />
-        Par exemple vous allez apprendre ce qu'est un serveur DNS, DHCP, comment
-        les pc peuvent communiquer entre eux, ...
-      </p>
-      <div className="imgBUT1">
-        <img src={lan} alt="Réseau Local de base" className="lan"></img>
+      <div className="introBUT1">
+        <h3>Le déroulement de la première année</h3>
+        <br></br>
+        <h5>
+          La première année est une introduction au monde des réseaux et
+          des télécommunications.<br></br> Elle a pour but de vous introduire dans cet univers
+          en vous apprenant les bases.
+        </h5>
+      </div>
+      <br></br><br></br>   
+      <div className="resBUT1">
+        <h3>Le réseau</h3>
+        <p>
+          Dans la partie réseau, vous allez apprendre ce qu'est concrètement un réseau,
+          assimiler son fonctionnement et configurer les différents équipements dont
+          il est composé. <br/>
+          Par exemple vous allez voir ce qu'est un serveur DNS, un serveur DHCP, la façon dont
+          les PC communiquent entre eux et bien d'autres choses encore !
+        </p>
+        <div className="imgBUT1">
+          <img src={lan} alt="Réseau Local de base" className="lan"></img>
+        </div>
       </div>
       <span>Partie téléphonie/signal :</span>
       <p>
