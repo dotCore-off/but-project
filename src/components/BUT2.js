d273642831fd4d6bbb348a4cf5c60e3c5893f5ce
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-19 17:03:52 +0100

    but2

--- src/components/BUT2.js
+++ src/components/BUT2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ldap from "../media/programme/BUT2/ldap.png";
 import vpn from "../media/programme/BUT2/vpn.svg";
-import web from "../media/programme/BUT1/web.png";
+import bdd from "../media/programme/BUT2/bdd.png";
 
 const BUT1 = () => {
   return (
@@ -31,33 +31,25 @@ const BUT1 = () => {
       <span>Partie téléphonie/signal :</span>
       <p>
         Dans cette partie, vous allez apprendre à configurer les réseaux
-        opérateur et à crééer des accès distants (par exemple à créer un VPN). <br />
+        opérateur et à crééer des accès distants (par exemple à créer un VPN).{" "}
+        <br />
       </p>
       <div className="imgBUT2">
-        <img
-          src={vpn}
-          alt="tunnel vpn"
-          className="vpn"
-        ></img>
+        <img src={vpn} alt="tunnel vpn" className="vpn"></img>
       </div>
       <span>Partie Programmation :</span>
       <p>
-        Dans cette partie, vous allez apprendre les bases de la programmation
-        dans plusieurs langages. Dans la partie Linux, vous allez apprendre à
-        utiliser le système d’exploitation Ubuntu et notamment les commandes
-        principales du terminal.
+        Dans cette partie, vous allez appronfondire la création de script vue en
+        première année (script plus complexe).
         <br />
-        Pour la partie Programmation Web, vous allez apprendre à concevoir votre
-        premier site web en HTML CSS puis à travailler avec les bases de données
-        (SQL).
-        <br /> Vous apprendrez aussi à créer des programmes en C dans un premier
-        puis en C++.
+        Vous allez aussi appronfondire vos connaissance dans la conception et la
+        gestion de base de donnée.
       </p>
       <div className="imgBUT2">
         <img
-          src={web}
-          alt="Creation du site Web du BUT"
-          className="prograWeb"
+          src={bdd}
+          alt="Requête SQL dans une base de donnée"
+          className="bdd"
         ></img>
       </div>
     </div>
