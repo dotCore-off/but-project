bb86c9c181d3f624484ab81f15f8a5f063e14546
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-19 18:22:08 +0100

    but3 v1

--- src/components/BUT3.js
+++ src/components/BUT3.js
@@ -1,6 +1,6 @@
 import React from "react";
-import lan from "../media/programme/BUT1/lan.png";
-import telephonie from "../media/programme/BUT1/telephonie.png";
+import infra from "../media/programme/BUT3/infra.png";
+import gsm from "../media/programme/BUT3/gsm.png";
 import web from "../media/programme/BUT1/web.png";
 
 const BUT3 = () => {
@@ -14,42 +14,36 @@ const BUT3 = () => {
       </p>
       <span>Partie Réseaux :</span>
       <p>
-        Dans la partie réseaux, vous allez apprendre ce qu'est un réseau,
-        comment ça fonctionne et comment configurer les différents appareil qui
-        composent un réseau local. <br />
-        Par exemple vous allez apprendre ce qu'est un serveur DNS, DHCP, comment
-        les pc peuvent communiquer entre eux, ...
-      </p>
-      <div className="imgBUT3">
-        <img src={lan} alt="Réseau Local de base" className="lan"></img>
-      </div>
-      <span>Partie téléphonie/signal :</span>
-      <p>
-        Dans cette partie, vous allez apprendre à mesurer et analyser des
-        signaux. Vous allez donc voir comment ce transmet une information à
-        travers un signal. <br />
-        Vous allez ensuite voir les différent support de transmission (cuivre,
-        fibre optique) et configurer des téléphones.
+        Dans la partie réseau, vous devrez être en mesure de concevoir un réseau
+        d’entreprise tout en assurant la haute disponibilité de ce réseau.
+        <br />
+        Vous devrez pouvoir créer une maquette et défendre votre projet.
       </p>
       <div className="imgBUT3">
         <img
-          src={telephonie}
-          alt="Configuration de téléphonie  Alcatel"
-          className="telephonie"
+          src={infra}
+          alt="Partie d'un schéma d'une infrastructure réseau"
+          className="infra"
+        ></img>
+      </div>
+      <span>Partie téléphonie/signal :</span>
+      <p>
+        Dans cette partie, vous allez apprendre à déployer un réseau d’accès
+        sans-fil pour les entreprises et à déployer un réseau d’accès fixes et
+        mobiles pour les opérateurs (mise en place d’antennes par exemple).
+      </p>
+      <div className="imgBUT3">
+        <img
+          src={gsm}
+          alt="Schéma d'une infrastructure GSM"
+          className="gsm"
         ></img>
       </div>
       <span>Partie Programmation :</span>
       <p>
-        Dans cette partie, vous allez apprendre les bases de la programmation
-        dans plusieurs langages. Dans la partie Linux, vous allez apprendre à
-        utiliser le système d’exploitation Ubuntu et notamment les commandes
-        principales du terminal.
-        <br />
-        Pour la partie Programmation Web, vous allez apprendre à concevoir votre
-        premier site web en HTML CSS puis à travailler avec les bases de données
-        (SQL).
-        <br /> Vous apprendrez aussi à créer des programmes en C dans un premier
-        puis en C++.
+        Dans cette partie, vous allez apprendre à mettre en place un
+        environnement de travail collaboratif et à élaborer le cahier des
+        charges d'une application informatique.
       </p>
       <div className="imgBUT3">
         <img
