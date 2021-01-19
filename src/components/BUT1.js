0f73951c465189ee7b84488918bc2f364dd9c24c
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-01-19 21:23:34 +0100

    Updated some textual things.

--- src/components/BUT1.js
+++ src/components/BUT1.js
@@ -9,22 +9,24 @@ const BUT1 = () => {
       <div className="introBUT1">
         <h3>Le déroulement de la première année</h3>
         <br></br>
-        <h5>
+        <h4>
           La première année est une introduction au monde des réseaux et
           des télécommunications.<br></br> Elle a pour but de vous introduire dans cet univers
           en vous apprenant les bases.
-        </h5>
+        </h4>
       </div>
-      <br></br><br></br>   
+      <br/><br/>
       <div className="resBUT1">
         <h3>Le réseau</h3>
-        <p>
+        <br/>
+        <h4>
           Dans la partie réseau, vous allez apprendre ce qu'est concrètement un réseau,
           assimiler son fonctionnement et configurer les différents équipements dont
           il est composé. <br/>
           Par exemple vous allez voir ce qu'est un serveur DNS, un serveur DHCP, la façon dont
           les PC communiquent entre eux et bien d'autres choses encore !
-        </p>
+        </h4>
+        <br/>
         <div className="imgBUT1">
           <img src={lan} alt="Réseau Local de base" className="lan"></img>
         </div>
