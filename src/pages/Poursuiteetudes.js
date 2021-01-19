77eb5640a939cf8e7f09dfa12c8aebcd6cb1709b
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-19 12:15:00 +0100

    test

--- src/pages/Poursuiteetudes.js
+++ src/pages/Poursuiteetudes.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
-import Diagoursuiteetude from '../components/Diagoursuiteetude';
+import React from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import Diagoursuiteetude from "../components/Diagoursuiteetude";
 
 const Poursuiteetudes = () => {
   return (
     <div className="poursuiteEtudes">
       <Navigation />
       <div className="poursuiteEtudesContent">
-      <Diagoursuiteetude />
+        <Diagoursuiteetude />
       </div>
       <Footer />
     </div>
