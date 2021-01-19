95a39d1dc67da689d6248114948210f28bbf107e
TriinL <77109126+TriinL@users.noreply.github.com>
2021-01-19 23:47:55 +0100

    ROM page 50%
    Need to update scss and probably the second part.

---
+++ src/components/ROM.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const ROM = () => {
+  return (
+    <div className="divROM">
+      <div className="introROM">
+        <h3>Qu'est ce que le spécialisation Réseaux Opérateurs et Multimédia (ROM) ?</h3>
+        <br></br>
+        <p>
+          Cette spécialisation a pour but d'approfondir vos connaissances acquises au long des années précédentes
+        <br></br>
+      </p>
+      </div>
+      <br/><br/>
+      <div className="...">
+        <h3>Infrastructure et services réseaux opérateurs</h3>
+        <br/>
+        <p>
+          Dans cette partie, vos connaissances en réseaux seront mises à l'épreuve.<br></br>
+          Par exemple, savez-vous virtualiser un réseau, comment marche le WIFI, qu'est ce qu'un routage ?<br></br>
+          ou encore vous êtes un flemmard stratégique et des scripts pourrez-vous aider dans cette quête.<br></br>
+          Voilà ce que peut vous offrir cette spécialisation. 
+        </p>
+        <br/>
+        <div className="...">
+          <img src={"..."} 
+          alt="...." 
+          className="..."
+          ></img>
+        </div>
+      </div>
+      <div className="...">
+      <h3>Communication et vidéo :</h3>
+      <p>
+        Dans cette partie, vous mettrez en place un service ToIP téléphonique pour une entreprise<br></br>
+        ou encore un service de vidéo-surveillance via des matériels comme des IPbx.<br></br>
+        Vous serez en charge de configurer ce matériel et de la maintenance de cette installation. 
+      </p>
+      </div>
+      <div className="...">
+        <img
+          src={"..."}
+          alt="..."
+          className="..."
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default ROM;
