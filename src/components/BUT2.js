ca89725a21edc5134b2fbf1bf954e17e409141d2
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-01-20 03:04:53 +0100

    Finished second section of BUT.

--- src/components/BUT2.js
+++ src/components/BUT2.js
@@ -6,51 +6,66 @@ import bdd from "../media/programme/BUT2/bdd.png";
 const BUT2 = () => {
   return (
     <div className="divBUT2">
-      <h3>Le déroulement de la deuxième année</h3>
-      <p>
-        La deuxième année est un approfondissement de la première année sur le
-        monde des réseaux et télécommunication.
-      </p>
-      <span>Partie Réseaux :</span>
-      <p>
-        Dans la partie réseaux, vous allez apprendre le fonctionnement des
-        réseaux opérateur, comment votre fournisseur d'accès internet vous
-        donnes accès à internet.
-        <br />
-        Vous allez par exemple apprendre à configurer des appareils opérateur, à
-        déployer des service réseaux avancés (configuration de windows serveur
-        2016).
-      </p>
-      <div className="imgBUT2">
-        <img
-          src={ldap}
-          alt="Configuration d'un annuaire LDAP"
-          className="ldap"
-        ></img>
+      <div className="introBUT2">
+        <h3>Le déroulement de la deuxième année</h3>
+        <br></br>
+        <h4>
+          La deuxième année est un approfondissement de la première année dans le
+          monde des réseaux et télécommunications.
+        </h4>
       </div>
-      <span>Partie téléphonie/signal :</span>
-      <p>
-        Dans cette partie, vous allez apprendre à configurer les réseaux
-        opérateur et à crééer des accès distants (par exemple à créer un VPN).{" "}
-        <br />
-      </p>
-      <div className="imgBUT2">
-        <img src={vpn} alt="tunnel vpn" className="vpn"></img>
+      <br/><br/>
+      <div className="resBUT2">
+        <h3>Le réseau</h3>
+        <br/>
+        <h4>
+          Dans la partie réseau, vous allez voir le fonctionnement des
+          réseaux opérateurs, c'est à dire la manière dont un FAI vous donne 
+          l'accès à Internet.
+          <br />
+          Vous allez par exemple apprendre à configurer des appareils opérateurs ou encore
+          à déployer des services réseaux avancés (Windows serveur 2016, ...).
+        </h4>
+        <br/>
+        <div className="imgBUT2">
+          <img
+            src={ldap}
+            alt="Configuration d'un annuaire LDAP"
+            className="ldap"
+          ></img>        
+        </div>
       </div>
-      <span>Partie Programmation :</span>
-      <p>
-        Dans cette partie, vous allez appronfondire la création de script vue en
-        première année (script plus complexe).
+      <div className="telBUT2">
+        <br /><br />
+        <h3>La télécommunication</h3>
         <br />
-        Vous allez aussi appronfondire vos connaissance dans la conception et la
-        gestion de base de donnée.
-      </p>
-      <div className="imgBUT2">
-        <img
-          src={bdd}
-          alt="Requête SQL dans une base de donnée"
-          className="bdd"
-        ></img>
+        <h4>
+          Au niveau de la télécommunication, vous apprendrez à créer et configurer des réseaux
+          opérateurs ainsi que la mise en place des accès distants (création d'un VPN, ...).<br />
+        </h4>
+        <br />
+        <div className="imgBUT2">
+          <img src={vpn} alt="tunnel vpn" className="vpn"></img>
+        </div>
+      </div>
+      <div className="progBUT2">
+        <br /><br />
+        <h3>La programmation</h3>
+        <br />
+        <h4>
+          Dans cette partie, vous allez appronfondir la création de scripts vue en
+          première année (mise en place de tâches, scripts de sauvegarde / restauration).<br />
+          Enfin, vous aurez l'occasion de peaufiner vos connaissances dans la conception et la
+          gestion de base de données.        
+        </h4>
+        <br />
+        <div className="imgBUT2">
+          <img
+            src={bdd}
+            alt="Requête SQL dans une base de donnée"
+            className="bdd"
+          ></img>
+        </div>
       </div>
     </div>
   );
