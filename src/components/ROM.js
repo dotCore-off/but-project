1c6e6215707615abc619041ed101611332fdc640
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-20 01:00:57 +0100

    ajout images rom

--- src/components/ROM.js
+++ src/components/ROM.js
@@ -1,4 +1,6 @@
 import React from "react";
+import appliweb from "../media/programme/ROM/appliweb.png";
+import VoIP from "../media/programme/ROM/VoIP.png";
 
 const ROM = () => {
   return (
@@ -23,29 +25,45 @@ const ROM = () => {
         <br />
         <p>
           Dans cette partie, vos connaissances en réseaux seront mises à
-          l'épreuve.<br />
-          Par exemple, savez-vous comment fonctionne le Cloud, comment configurer un IPBx,
-          qu'est ce qu'un routage ?<br />
-          ou encore vous êtes un flemmard stratégique et des scripts
-          pourrez-vous aider dans cette quête.<br />
+          l'épreuve.
+          <br />
+          Durant la première année, vous verez comment fonctionne le routage au
+          seins d'un réseaux ou comment virtualiser des service réseaux.
+          <br />
+          Pour ce qui est de la deuxième année, vous verrez comment fonctionne
+          le Cloud, comment configurer un IPBx, ou encore ce qu'est le WebRTC.
+          <br />
           Voilà ce que peut vous offrir cette spécialisation.
         </p>
         <br />
-        <div className="...">
-          <img src={"..."} alt="...." className="..."></img>
+        <div className="imgRom">
+          <img
+            src={appliweb}
+            alt="Mise en accès distant web des application thunderbird et wireshark"
+            className="appliWeb"
+          ></img>
         </div>
       </div>
       <div className="...">
         <h3>Communication et vidéo :</h3>
         <p>
-          Dans cette partie, vous mettrez en place des service de visio
-          conférence pour les entreprise ou encore un service de vidéo-surveillance.<br />
+          Durant la première année, vous apprendrez à administrer les services
+          de voix et de vidéos d'un opérateur ainsi qu'a programmer le réseaux.{" "}
+          <br />
+          Durant la deuxième année, vous mettrez en place des service de visio
+          conférence pour les entreprise ou encore un service de
+          vidéo-surveillance.
+          <br />
           Vous serez en charge d'administrer les services de communication des
           entreprises (configuration avancée des IPBX par exemple).
         </p>
       </div>
-      <div className="...">
-        <img src={"..."} alt="..." className="..."></img>
+      <div className="imgRom">
+        <img
+          src={VoIP}
+          alt="Création de profiles pour la VoIP et l'accès internet"
+          className="voip"
+        ></img>
       </div>
     </div>
   );
