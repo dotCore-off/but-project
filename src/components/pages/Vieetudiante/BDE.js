45f56557d7f51758d416a7e2f5bf8e8b781ccee8
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-26 17:19:51 +0100

    début scss BDE

--- src/components/pages/Vieetudiante/BDE.js
+++ src/components/pages/Vieetudiante/BDE.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import bde from '../../../media/bde/bde.jpg'
+import bde from "../../../media/bde/bde.jpg";
 
 const BDE = () => {
   return (
@@ -9,9 +9,14 @@ const BDE = () => {
         <h2>Notre BDE</h2>
         <p>
           Le Bureau des Etudiants est là pour créer une cohésion entres les
-          étudiants. Il organise des soirées, des animations, ...
+          étudiants.<br /> Il organise des soirées, des animations, participe à la vente de Sweat-shirt...
         </p>
-        <img src={bde} alt="logo BDE" className="logoBDE" />
+        <div className="imgBDE">
+          <img src={bde} alt="logo BDE" className="logoBDE" />
+        </div>
+        <div className="reseauBDE">
+          <span>Leur réseaux sociaux</span>
+        </div>
       </div>
     </div>
   );
