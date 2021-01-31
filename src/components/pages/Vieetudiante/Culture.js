c3fddc51b5ab215df24c3dd2427d788e6874d934
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-01-31 11:09:45 +0100

    fondDiscord.jpg

--- src/components/pages/Vieetudiante/Culture.js
+++ src/components/pages/Vieetudiante/Culture.js
@@ -10,9 +10,12 @@ const Culture = () => {
             La Maison du campus permet aussi aux étudiants d’accéder à la
             culture. En effet, elle met en œuvre différents partenariats pour
             permettre aux étudiants d’accéder à cette dernière à moindre coup.
-            Elle met par exemple des <em title="La valeur totale du carnet est de 32€ avec un prix de vente du carnet est de 16€.">carnets culture</em> qui sont des coupons
-            utilisables dans plus de 60 établissements culturels et qui vous
-            permettent de faire des économies.
+            Elle met par exemple des{" "}
+            <em title="La valeur totale du carnet est de 32€ avec un prix de vente du carnet est de 16€.">
+              carnets culture
+            </em>{" "}
+            qui sont des coupons utilisables dans plus de 60 établissements
+            culturels et qui vous permettent de faire des économies.
           </p>
           <div className="liensMC">
             <a
