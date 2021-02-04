dffd7fb4f429bda6ead67173e229fed67703e5f4
NathanRodet <64322615+NathanRodet@users.noreply.github.com>
2021-02-04 23:20:58 +0100

    up

--- src/components/pages/Poursuitesetudes/Apreslebut.js
+++ src/components/pages/Poursuitesetudes/Apreslebut.js
@@ -1,17 +1,22 @@
 import React from 'react';
-// import inge from "../../../media/iut360/IUT360.jpg";
+import inge from "../../../media/apresBUT/inge.jpg";
+import apres_but from "../../../media/apresBUT/apres_but.jpg";
+import insertion_professionel from "../../../media/apresBUT/insertion_pro.jpg";
 
 
 const Apreslebut = () => {
   return (
     <div className="ApreslebutContent">
       <section className="general">
+        <h2>où se dirige les étudiants à la fin du but</h2>
 
       </section>
       <section className="cycleingenieur">
+        <h2>Title</h2>
 
       </section>
       <section className="insertion">
+        <h2>Title</h2>
 
       </section>
     </div>
