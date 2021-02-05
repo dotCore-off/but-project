69641aa8ebc2d5ce90cda072af09e2bdc1d7fd47
NathanRodet <64322615+NathanRodet@users.noreply.github.com>
2021-02-05 20:32:39 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navigation from '../components/others/Navigation';
 import Footer from '../components/others/Footer';
-import Banniere from '../components/pages/Home/Banniere';
-import ChoisirBUT from '../components/pages/Home/ChoisirBUT';
+// import Banniere from '../components/pages/Home/Banniere';
+// import ChoisirBUT from '../components/pages/Home/ChoisirBUT';
 import Contacts from '../components/pages/Home/Contacts';
-import Carousel from '../components/pages/Home/Carousel';
+// import Carousel from '../components/pages/Home/Carousel';
 import Banner from '../components/pages/Home/Banner';
 import ChoisirRoanne from '../components/pages/Home/ChoisirRoanne';
 import NosAvantages from '../components/pages/Home/NosAvantages';
